2bb171bfee87657b8676c98b25a2a3b17ebe10f8
Reseda <pro@pro.local>
2023-10-30 10:09:29 +0400

    Add homework3

---
+++ src/Sem_3/Numbers.java
@@ -0,0 +1,12 @@
+package Sem_3;
+
+// HW 3.1. Нужно покрыть тестами метод на 100%
+// Метод проверяет, является ли целое число записанное в переменную n, чётным (true) либо нечётным (false).
+
+
+public class Numbers {
+
+    public boolean evenOddNumberCheck(int number){
+        return number % 2 ==0;
+    }
+}
