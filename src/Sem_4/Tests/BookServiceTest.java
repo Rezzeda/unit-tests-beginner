e2609dfea96ae7f369c1336672878e25cd02a717
Reseda <pro@pro.local>
2023-10-30 13:57:49 +0400

    Add homework4

---
+++ src/Sem_4/Tests/BookServiceTest.java
@@ -0,0 +1,47 @@
+import Sem_4.book.Book;
+import Sem_4.book.BookRepository;
+import Sem_4.book.BookService;
+
+
+import Sem_4.book.InMemoryBookRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookServiceTest {
+    private BookRepository bookRepository;
+    private BookService bookService;
+
+    @BeforeEach
+    public void setUp() {
+        bookRepository = mock(InMemoryBookRepository.class);
+        bookService = new BookService(bookRepository);
+    }
+
+    @Test
+    public void testFindBookById() {
+        Book book = new Book("1", "Сказки", "Пушкин");
+        when(bookRepository.findById("1")).thenReturn(book);
+        Book testBook = bookService.findBookById("1");
+        assertEquals(book, testBook);
+        verify(bookRepository, times(1)).findById("1");
+    }
+
+    @Test
+    public void testFindAllBook() {
+        Book book1 = new Book("1", "Идиот", "Достоевский");
+        Book book2 = new Book("2", "Война и мир", "Толстой");
+        List<Book> books = new ArrayList<>(List.of(book1, book2));
+        when(bookRepository.findAll()).thenReturn(books);
+        List<Book> listOfBooks = bookService.findAllBooks();
+        assertEquals(books, listOfBooks);
+        verify(bookRepository, times(1)).findAll();
+    }
+}
